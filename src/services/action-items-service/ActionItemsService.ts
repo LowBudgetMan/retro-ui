3a9c26f1bb015c39f34d516cea4834c344b624bb
Nick Reuter <thenewimagineer@gmail.com>
2025-10-19 12:27:17 -0400

    Add central config for ActionItemsService base url

--- src/services/action-items-service/ActionItemsService.ts
+++ src/services/action-items-service/ActionItemsService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { DateTime } from "luxon";
+import {ApiConfig} from '../../config/ApiConfig.ts';
 
 export interface ActionItem {
     id: string;
@@ -11,36 +12,36 @@ export interface ActionItem {
 }
 
 async function getActionItems(teamId: string): Promise<ActionItem[]> {
-    return axios.get(`http://localhost:8080/api/teams/${teamId}/action-items`).then(response => response.data.map(transformActionItem));
+    return axios.get(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items`).then(response => response.data.map(transformActionItem));
 }
 
 async function createActionItem(teamId: string, action: string, assignee: string) {
-    return await axios.post(`http://localhost:8080/api/teams/${teamId}/action-items`, {
+    return await axios.post(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items`, {
         action,
         assignee
     });
 }
 
 async function setCompleted(teamId: string, actionItemId: string, completed: boolean) {
-    return await axios.put(`http://localhost:8080/api/teams/${teamId}/action-items/${actionItemId}/completed`, {
+    return await axios.put(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items/${actionItemId}/completed`, {
         completed
     })
 }
 
 async function setAction(teamId: string, actionItemId: string, action: string) {
-    return await axios.put(`http://localhost:8080/api/teams/${teamId}/action-items/${actionItemId}/action`, {
+    return await axios.put(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items/${actionItemId}/action`, {
         action
     });
 }
 
 async function setAssignee(teamId: string, actionItemId: string, assignee: string) {
-    return await axios.put(`http://localhost:8080/api/teams/${teamId}/action-items/${actionItemId}/assignee`, {
+    return await axios.put(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items/${actionItemId}/assignee`, {
         assignee
     })
 }
 
 async function deleteActionItem(teamId: string, actionItemId: string) {
-    return axios.delete(`http://localhost:8080/api/teams/${teamId}/action-items/${actionItemId}`);
+    return axios.delete(`${ApiConfig.baseApiUrl}/api/teams/${teamId}/action-items/${actionItemId}`);
 }
 
 export function transformActionItem(actionItem: ActionItem): ActionItem {
