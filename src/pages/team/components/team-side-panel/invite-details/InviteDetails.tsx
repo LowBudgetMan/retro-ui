747991ebba827960acdf38ade14b1a7a7efe7422
Nick Reuter <thenewimagineer@gmail.com>
2026-01-05 10:38:14 -0500

    Create Invite accordion component

---
+++ src/pages/team/components/team-side-panel/invite-details/InviteDetails.tsx
@@ -0,0 +1,31 @@
+import {InviteListItem} from "../../invite-list-item/InviteListItem.tsx";
+import {Invite, TeamService} from "../../../../../services/team-service/TeamService.ts";
+import {useRevalidator} from "react-router-dom";
+
+interface Props {
+    id: string;
+    name: string;
+    invites: Invite[];
+}
+
+export function InviteDetails({id, name, invites}: Props) {
+    const revalidator = useRevalidator();
+
+    const handleCreateInvite = async () => {
+        await TeamService.createInvite(id).then(() => revalidator.revalidate());
+    }
+
+    return (
+        <details>
+            <summary><h2>Invites</h2></summary>
+            <button onClick={handleCreateInvite}>Create invite link</button>
+            <ol>
+                {invites.map((invite) => (
+                    <li key={invite.id}>
+                        <InviteListItem invite={invite} teamId={id} teamName={name}/>
+                    </li>
+                ))}
+            </ol>
+        </details>
+    )
+}
