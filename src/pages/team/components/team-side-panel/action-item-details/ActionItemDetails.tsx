b3293d8aac56b03c77ed51fba69900794a24fdb8
Nick Reuter <thenewimagineer@gmail.com>
2026-01-05 15:40:20 -0500

    Add styling for the team page accordions

--- src/pages/team/components/team-side-panel/action-item-details/ActionItemDetails.tsx
+++ src/pages/team/components/team-side-panel/action-item-details/ActionItemDetails.tsx
@@ -1,12 +1,17 @@
 import {ActionItemsList} from "../../../../retro/components/action-items/action-items-list/ActionItemsList.tsx";
 import {useActionItems} from "../../../../../context/hooks.tsx";
+import accordionStyles from '../Accordion.module.css';
 
 export function ActionItemDetails() {
     const {actionItems} = useActionItems();
     return (
-        <details open={true}>
-            <summary><h2>Action Items</h2></summary>
-            <ActionItemsList actionItems={actionItems} />
+        <details className={accordionStyles.sidePaneAccordion} open={true}>
+            <summary className={accordionStyles.sidePaneAccordionHeader}>
+                <h2 className={accordionStyles.sidePaneAccordionHeaderTitle}>Action Items</h2>
+            </summary>
+            <div className={accordionStyles.sidePaneAccordionContent}>
+                <ActionItemsList actionItems={actionItems} />
+            </div>
         </details>
     )
 }
