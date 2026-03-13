e6d47415069472a1d8804eb57fc79c9f4d6b0d5b
Matt Trueblood <50777673+mtruebl1@users.noreply.github.com>
2026-03-12 22:01:01 -0400

    Sort thoughts Tooltip (#19)

--- src/pages/retro/components/column-header/ColumnHeader.tsx
+++ src/pages/retro/components/column-header/ColumnHeader.tsx
@@ -2,6 +2,7 @@ import {CategoryStyling} from "../retro-column/RetroColumn.tsx";
 import {Category} from "../../../../services/retro-service/RetroService.ts";
 import styles from './ColumnHeader.module.css';
 import {BsCaretDown, BsCaretDownFill} from "react-icons/bs";
+import {useEffect, useRef, useState} from "react";
 
 interface ColumnHeaderProps {
     category: Category;
@@ -11,6 +12,33 @@ interface ColumnHeaderProps {
 }
 
 export function ColumnHeader({category, styling, isSorting, toggleSort}: ColumnHeaderProps) {
+    const [isTooltipVisible, setTooltipVisible] = useState<boolean>(false);
+    const hoverTimeout = useRef<NodeJS.Timeout | null>(null);
+    const delay = 1000;
+    const tooltipText = isSorting ? 'Sort by time added' : 'Sort by number of votes';
+
+    const handleMouseEnter = () => {
+        if (hoverTimeout.current) {
+            clearTimeout(hoverTimeout.current);
+        }
+        hoverTimeout.current = setTimeout(() => {
+            setTooltipVisible(true);
+        }, delay);
+    }
+
+    const handleMouseLeave = () => {
+        if (hoverTimeout.current) {
+            clearTimeout(hoverTimeout.current);
+            hoverTimeout.current = null;
+        }
+        setTooltipVisible(false);
+    }
+
+    useEffect(() => () => {
+        if (hoverTimeout.current) clearTimeout(hoverTimeout.current);
+    }, []);
+
+
     return (
         <>
             <h2 className={styles.categoryName} style={{
@@ -22,10 +50,19 @@ export function ColumnHeader({category, styling, isSorting, toggleSort}: ColumnH
                         className={styles.sortButton}
                         aria-label={`Sort ${category.name} by ${isSorting ? 'time' : 'votes'}`}
                         onClick={toggleSort}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
                     >
                     {isSorting
                         ? <BsCaretDownFill className={styles.sortIcon} color={styling.textColor}/>
                         : <BsCaretDown className={styles.sortIcon} color={styling.textColor}/>}
+                    <span
+                        id="sort-tooltip"
+                        role="tooltip"
+                        className={isTooltipVisible ? styles.tooltip : styles.tooltipHidden}
+                    >
+                        {tooltipText}
+                    </span>
                 </button>
             </h2>
         </>
